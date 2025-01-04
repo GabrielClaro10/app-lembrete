7573dddd099c72fbd1ad02465bb7b8ec301ccac7
Gabriel Claro <gabrielclaro@gmail.com>
2025-01-04 10:14:18 -0300

    update

--- frontend/lib/src/modules/login/login.dart
+++ frontend/lib/src/modules/login/login.dart
@@ -4,7 +4,6 @@ import 'package:get/get_state_manager/src/simple/get_view.dart';
 import 'package:lembra_mais/src/modules/login/login_controller.dart';
 import 'package:lembra_mais/src/modules/esqueceuSenha/esqueceuSenha.dart';
 import 'package:lembra_mais/src/modules/cadastroUsers/cadastrarUser.dart';
-import 'package:lembra_mais/src/modules/home/home.dart';
 
 class Login extends GetView<LoginController> {
   @override
